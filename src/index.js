d455a486167377fa286d0a1ee165b7d937266b46
Lucas Scommegna <scommegnal@hotmail.com>
2024-01-01 14:44:58 -0300

    feat: Pizza component initial props testing

--- src/index.js
+++ src/index.js
@@ -57,6 +57,19 @@ function App() {
   );
 }
 
+function Pizza(props) {
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
+      </div>
+    </div>
+  );
+}
+
 function Header() {
   return (
     <header className="header">
@@ -69,9 +82,12 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu:</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Focaccia"
+        ingredients="Bread with italian olive oil and rosemary"
+        photoName="pizzas/focaccia.jpg"
+        price={10}
+      />
     </main>
   );
 }
@@ -90,16 +106,6 @@ function Footer() {
     <footer className="footer">
       {new Date().toLocaleTimeString()}. We're currently open!
     </footer>
-  );
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/focaccia.jpg" alt="focaccia" />
-      <h3>Focaccia</h3>
-      <p>Bread with italian olive oil and rosemary</p>
-    </div>
   );
 }
 
