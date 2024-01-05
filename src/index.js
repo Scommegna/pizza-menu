e997d6929d6f6cc1415cb33c4dba4bc3d91f85e0
Lucas Scommegna <scommegnal@hotmail.com>
2024-01-05 20:10:24 -0300

    feat: conditional rendering for sold out pizzas

--- src/index.js
+++ src/index.js
@@ -58,6 +58,8 @@ function App() {
 }
 
 function Pizza(props) {
+  if (props.pizzaObj.soldOut) return null;
+
   return (
     <li className="pizza">
       <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
