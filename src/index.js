0af115b4324aa2708cfd69f7827e7fab24939264
Lucas Scommegna <scommegnal@hotmail.com>
2024-01-05 20:06:40 -0300

    refactor: change to ternary operator to conditional rendering

--- src/index.js
+++ src/index.js
@@ -86,12 +86,14 @@ function Menu() {
     <main className="menu">
       <h2>Our menu:</h2>
 
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzas.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p>We're still working in our menu. Please, come back later.</p>
       )}
     </main>
   );
@@ -106,11 +108,15 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We're open until {closeHour}:00. Come visit us or order online.</p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
       )}
     </footer>
   );
